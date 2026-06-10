2067a861454e4d75239d3d9aa0a0df858a939905
bernhardblietz <bernhard.blietz@mailbox.org>
2026-06-10 09:29:30 +0200

    style Button using material UI

--- packages/web/src/components/ui/Button.tsx
+++ packages/web/src/components/ui/Button.tsx
@@ -1,3 +1,5 @@
+import { Button as MuiButton } from "@mui/material"
+
 type ButtonProps = {
   variant: "primary" | "danger";
   type?: "button" | "submit";
@@ -8,8 +10,8 @@ type ButtonProps = {
 
 export default function Button({ variant, type = "button", onClick, disabled, children }: ButtonProps) {
   return (
-    <button type={type} onClick={onClick} disabled={disabled} className={`px-4 py-2 rounded ${variant} ${disabled ? "opacity-50 pointer-events-none" : ""}`}>
-        <div>{children}</div>
-    </button>
+    <MuiButton type={type} onClick={onClick} disabled={disabled} className={variant} >
+        {children}
+    </MuiButton>
   )
 }
